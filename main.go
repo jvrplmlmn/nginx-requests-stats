f556f375693b9892f5c69132c213ab7e96943f4f
Javier Palomo Almena <javier.palomo.almena@gmail.com>
2017-02-02 11:49:06 +0100

    Update log format to parse logs with upstream and cache fields by default

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ var format string
 var logFile string
 
 func init() {
-	flag.StringVar(&format, "format", `$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent"`, "Log format")
+	flag.StringVar(&format, "format", `$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" $upstream_addr $upstream_cache_status`, "Log format")
 	flag.StringVar(&logFile, "log", "/var/log/nginx/access.log", "Log file name to read.")
 }
 
